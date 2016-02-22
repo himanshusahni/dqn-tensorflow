4066c61d5ff6ffc495628e04e3826959ecf534e6
himanshusahni <himanshu@gatech.edu>
2016-02-21 23:54:07 -0500

    made output of play prettier

--- src/play.py
+++ src/play.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 import dqn
 import domains
@@ -9,9 +10,15 @@ if __name__ == "__main__":
     #create session and agent
     sess = tf.Session()
     agent = dqn.dqn(sess, domains.fire_fighter)
+    actions = agent.env.get_actions()
     #load previous agent
     saver = tf.train.Saver()
     model = tf.train.get_checkpoint_state("./models/")
+    if model and model.model_checkpoint_path:
+        print "Loading model " + model.model_checkpoint_path
+        saver.restore(sess, model.model_checkpoint_path)
+
+    #set up GUI
     plt.ion()
     f = plt.figure()
     a = f.add_subplot(131)
@@ -20,14 +27,12 @@ if __name__ == "__main__":
     plt.title("state-1")
     c = f.add_subplot(133)
     plt.title("state")
-    print agent.img_size
     a_show = a.imshow(np.zeros(agent.img_size))
     b_show = b.imshow(np.zeros(agent.img_size))
     c_show = c.imshow(np.zeros(agent.img_size))
     plt.show()
+
     input_map = {'a': 0, 'd': 1, 'w': 2, 's': 3, 'z': 4, 'x': 5}
-    if model and model.model_checkpoint_path:
-        saver.restore(sess, model.model_checkpoint_path)
     inp = ""
     with sess.as_default():
         while (inp != "q"):
@@ -44,17 +49,25 @@ if __name__ == "__main__":
                 c_show.autoscale()
                 #pick best action according to convnet on current state
                 action_values = agent.train_net.logits.eval(feed_dict={agent.batch_state_placeholder: np.expand_dims(agent.state, axis=0)})
+                action_values = np.squeeze(action_values)
                 max_a = np.argmax(action_values)
-                print action_values
-                print max_a
+                for i,value in enumerate(action_values):
+                    print(actions[i] + " value: " + str(value))
+                print "Action preferred by net: " + actions[max_a]
                 plt.draw()
-                inp = raw_input("Selection action to take or q to exit. To auto-pilot press y")
-                if inp == 'q':
-                    break
-                elif inp == 'y':
-                    chosen_a = max_a
-                else:
-                    chosen_a = input_map[inp]
+                invalid = True
+                while invalid:
+                    invalid = False
+                    inp = raw_input("Selection action to take or q to exit. To auto-pilot press y\n")
+                    if inp == 'q':
+                        break
+                    elif inp == 'y':
+                        chosen_a = max_a
+                    elif (inp in input_map):
+                        chosen_a = input_map[inp]
+                    else:
+                        print "Invalid selection, please select: w,s,a,d,z,x,q, or y only!"
+                        invalid = True
                 #execute that action in the environment,
                 (next_state, reward, terminal) = agent.env.take_action(chosen_a, "False")
                 agent.state = next_state
